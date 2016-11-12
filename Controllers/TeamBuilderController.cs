7302625c1b4b0abd7471e22cd70227659c25267b
Kemal Gültekin <kemal@emakina.nl>
2016-11-12 20:53:46 +0300

    * implemented player adding functionality

--- Controllers/TeamBuilderController.cs
+++ Controllers/TeamBuilderController.cs
@@ -2,24 +2,41 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodeNamesHelper.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CodeNamesHelper.Controllers
 {
     public class TeamBuilderController : Controller
     {
-        List<String> players;
+        private static volatile List<Player> _players = new List<Player>();
 
-        
         public IActionResult Index()
         {
-            return View();
+            return View(_players);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Add(String playerName) {
-            
-            return View("Index");
+        public IActionResult Add(String playerName)
+        {
+            var lastPlayer = _players.LastOrDefault();
+
+            var newId = 1;
+            if (lastPlayer != null)
+            {
+                newId = lastPlayer.Id + 1;
+            }
+
+
+            var newPlayer = new Player
+            {
+                Id = newId,
+                Name = playerName
+            };
+
+            _players.Add(newPlayer);
+
+            return View("Index", _players);
         }
     }
 }
